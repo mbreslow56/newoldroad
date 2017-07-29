5cde470a3e62e2e29a1685a07197d4f3fb7560f0
Arkadi <mrarkadi@gmail.com>
2017-07-29 23:20:07 +0300

    added hamburger functionality

--- public/js/directives/navDirective.js
+++ public/js/directives/navDirective.js
@@ -1,5 +1,16 @@
 app.directive("navDirective", function(){
 	return {
 		templateUrl: 'nav.html',
+		link: function(scope) {
+      scope.showMenu = true;
+			scope.toggleMenu = function() {
+				scope.showMenu = !scope.showMenu;
+			}
+			scope.hideMenu = function() {
+    		if (document.getElementById('home_mobile').offsetParent !== null) { // if we're on mobile, activate this function
+					scope.showMenu = false;
+				}
+			}
+    }
 	};
 });
