e104f762b83511b6412f399e20bcf030867af9e2
Arkadi <mrarkadi@gmail.com>
2017-07-30 21:21:06 +0300

    nav_burger_end?

--- public/js/directives/navDirective.js
+++ public/js/directives/navDirective.js
@@ -1,16 +1,24 @@
-app.directive("navDirective", function(){
+app.directive("navDirective", ['$state', function(state){
 	return {
 		templateUrl: 'nav.html',
-		link: function(scope) {
-      scope.showMenu = true;
+		link: function(scope, attrs) {
+		  scope.showMenu = document.getElementById('home_mobile').offsetParent !== null ? false : true; // if mobile: don't show menu initially
 			scope.toggleMenu = function() {
-				scope.showMenu = !scope.showMenu;
+				if (document.getElementById('home_mobile').offsetParent !== null) { // if we're on mobile, activate this function
+					scope.showMenu = !scope.showMenu;
+				}
 			}
 			scope.hideMenu = function() {
     		if (document.getElementById('home_mobile').offsetParent !== null) { // if we're on mobile, activate this function
 					scope.showMenu = false;
 				}
 			}
+
+			scope.goHome = function() {
+				// $location.url('http://test.com/login.jsp?un');
+				console.log("hi");
+				state.go('home');
+			}
     }
 	};
-});
+}]);
