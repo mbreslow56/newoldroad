44eaa63d82aa8a30a2ab6a27de927e74d3ce39c3
mbreslow56 <mbreslow56@gmail.com>
2018-01-21 02:53:00 +0200

    more nav fixes

--- public/js/directives/navDirective.js
+++ public/js/directives/navDirective.js
@@ -1,24 +1,30 @@
-app.directive("navDirective", ['$state', function(state){
+app.directive("navDirective", function(){
 	return {
 		templateUrl: 'nav.html',
-		link: function(scope, attrs) {
-		  scope.showMenu = document.getElementById('home_mobile').offsetParent !== null ? false : true; // if mobile: don't show menu initially
-			scope.toggleMenu = function() {
-				if (document.getElementById('home_mobile').offsetParent !== null) { // if we're on mobile, activate this function
-					scope.showMenu = !scope.showMenu;
-				}
-			}
-			scope.hideMenu = function() {
-    		if (document.getElementById('home_mobile').offsetParent !== null) { // if we're on mobile, activate this function
-					scope.showMenu = false;
-				}
-			}
-
-			scope.goHome = function() {
-				// $location.url('http://test.com/login.jsp?un');
-				console.log("hi");
-				state.go('home');
-			}
-    }
 	};
-}]);
+});
+
+// app.directive("navDirective", ['$state', function(state){
+// 	return {
+// 		templateUrl: 'nav.html',
+// 		link: function(scope, attrs) {
+// 		  scope.showMenu = document.getElementById('home_mobile').offsetParent !== null ? false : true; // if mobile: don't show menu initially
+// 			scope.toggleMenu = function() {
+// 				if (document.getElementById('home_mobile').offsetParent !== null) { // if we're on mobile, activate this function
+// 					scope.showMenu = !scope.showMenu;
+// 				}
+// 			}
+// 			scope.hideMenu = function() {
+//     		if (document.getElementById('home_mobile').offsetParent !== null) { // if we're on mobile, activate this function
+// 					scope.showMenu = false;
+// 				}
+// 			}
+
+// 			scope.goHome = function() {
+// 				// $location.url('http://test.com/login.jsp?un');
+// 				console.log("hi");
+// 				state.go('home');
+// 			}
+//     }
+// 	};
+// }]);
